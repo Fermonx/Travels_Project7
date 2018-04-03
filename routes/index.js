84d0a88d9aa8d5db8b047e70e529f08f6df36c58
FernanMonc <fernandomdsatoutlook.com>
2018-04-03 17:38:02 +0200

    Removed bcrypt

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var userModel = require('../models/userModel');
-var bcrypt = require('bcrypt');
+
 
 
 /* GET home page. */
--- script.sh
+++ script.sh
@@ -5,21 +5,24 @@ apt-get install -y nodejs
 apt-get install -y nodejs-legacy
 apt-get install -y npm
 apt-get install -y git
-npm install forever
-apt-get install -y mysql-server php5-mysql
 debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
-apt-get -f install
+apt-get install -y mysql-server
 
 if [[ $(cd project) ]]
 then
-    git pull https://github.com/Fermonx/actividad_3.git
+    git pull https://github.com/Fermonx/actividad_4.git
 
 else
 mkdir project
 cd project || exit
-git clone https://github.com/Fermonx/actividad_3.git
+git clone https://github.com/Fermonx/actividad_4.git
 fi
 
-
-
+cd
+cd project
+cd actividad_4
+sudo npm install
+npm install bower
+npm install forever
+npm start
